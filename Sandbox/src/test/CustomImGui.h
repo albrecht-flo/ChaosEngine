f03fb3581c103d4ba197499fb52c5c7c85a6c6ab
Florian Albrecht <florian.t.albrecht@tum.de>
2021-03-29 14:13:52 +0200

    Moved Viewport into its own function

--- Sandbox/src/test/CustomImGui.h
+++ Sandbox/src/test/CustomImGui.h
@@ -1,10 +1,25 @@
 #pragma once
 
+#include "Engine/src/renderer/api/Framebuffer.h"
 #include <functional>
+#include <imgui.h>
 
 class Window;
-namespace CustomImGui {
-    void ImGuiEnableDocking(const std::function<void(void)>& menuCallback);
 
-    void RenderLogWindow();
-}
+class CustomImGui {
+private:
+    struct CustomImGuiState {
+        bool followLog = true;
+        ImVec2 previousSize = ImVec2(0.0f, 0.0f);
+    };
+
+public:
+    static void ImGuiEnableDocking(const std::function<void(void)> &menuCallback);
+
+    static void RenderLogWindow(const std::string &title = "Log");
+
+    static ImVec2 renderViewport(const Renderer::Framebuffer &framebuffer);
+
+private:
+    static CustomImGuiState state;
+};
