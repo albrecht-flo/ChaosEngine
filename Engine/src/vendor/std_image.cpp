d3564fbde265307951aecafa9fde46dd3fb3c8d7
Florian Albrecht <florian.albrecht.public@outlook.de>
2022-01-01 17:02:16 +0100

    Disabled warnings for third party library code

--- Engine/src/vendor/std_image.cpp
+++ Engine/src/vendor/std_image.cpp
@@ -1,3 +1,9 @@
+#include "platform.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 
+PUSH_MINIMAL_WARNING_LEVEL
+
 #include <stb_image.h>
+
+POP_MINIMAL_WARNING_LEVEL
--- Engine/src/vendor/vk_mem_allocator.cpp
+++ Engine/src/vendor/vk_mem_allocator.cpp
@@ -1,6 +1,12 @@
+#include "platform.h"
+
 #define VMA_IMPLEMENTATION
 #define VMA_STATIC_VULKAN_FUNCTIONS 1
 
+PUSH_MINIMAL_WARNING_LEVEL
+
 #include "vk_mem_alloc.h"
+
+POP_MINIMAL_WARNING_LEVEL
 
 // See https://gpuopen-librariesandsdks.github.io/VulkanMemoryAllocator/html/quick_start.html
