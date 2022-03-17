fe4cf9c173eeca7404b1caa4fedff8e677043640
Florian Albrecht <florian.albrecht.public@outlook.de>
2022-03-17 14:02:37 +0100

    WIP: Asset view

---
+++ Engine/src/core/assets/AssetManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by flori on 08.03.2022.
+//
+
+#include "AssetManager.h"
--- Sandbox/src/test/AssetView.cpp
+++ Sandbox/src/test/AssetView.cpp
@@ -1,16 +1,21 @@
 #include "AssetView.h"
-#include <imgui.h>
+#include "Engine/src/core/Utils/Logger.h"
 #include "CustomImGui.h"
+
+#include <imgui.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 using namespace CustomImGui;
 
 AssetView::AssetViewState AssetView::state = {};
 
 bool AssetView::renderAssetView() {
-    bool open = ImGui::Begin("Assets");
+    bool open = ImGui::Begin("Assets", nullptr, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
     if (open) {
         if (ImGui::BeginTable("table_nested1", 2,
-                              ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable)) {
+                              ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoClip)) {
 
             ImGui::TableNextColumn();
             ImGui::BeginChild("Tree panel");
@@ -27,7 +32,25 @@ bool AssetView::renderAssetView() {
 
             ImGui::TableNextColumn();
             ImGui::BeginChild("Assets panel");
-            ImGui::Text("Here will be the assets");
+            ImGui::Text("Assets");
+            fs::path assetPath("./Assets");
+            if (!fs::exists(assetPath)) {
+                create_directory(assetPath);
+                LOG_INFO("Created Directory {}", fs::absolute(assetPath).c_str());
+            }
+//            ImGui::Text("exists() = %s", fs::exists(assetPath) ? "true" : "false");
+//            ImGui::Text("dir() = %s", fs::is_directory(assetPath) ? "true" : "false");
+//            ImGui::Text("root_name() = %s", assetPath.root_name().c_str());
+//            ImGui::Text("root_path() = %s", assetPath.root_path().c_str());
+//            ImGui::Text("relative_path() = %s", assetPath.relative_path().c_str());
+//            ImGui::Text("parent_path() = %s", assetPath.parent_path().c_str());
+//            ImGui::Text("filename() = %s", assetPath.filename().c_str());
+//            ImGui::Text("stem() = %s", assetPath.stem().c_str());
+//            ImGui::Text("extension() = %s", assetPath.extension().c_str());
+            for (const auto &entry: fs::directory_iterator(assetPath)) {
+                ImGui::Text("%s%s", entry.path().c_str(), (entry.is_directory() ? "/" : ""));
+            }
+
             ImGui::EndChild();
 
             ImGui::EndTable();
