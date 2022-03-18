26779b9dc249b3b5e9418146c71f304c0b41f55d
Florian Albrecht <florian.t.albrecht@tum.de>
2022-03-18 18:33:35 +0100

    Added Meshes to engine asset management

--- Engine/src/core/assets/AssetManager.h
+++ Engine/src/core/assets/AssetManager.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <memory>
 #include <string>
 #include <unordered_map>
 
+#include "Engine/src/renderer/api/RenderMesh.h"
 #include "Engine/src/renderer/api/Material.h"
 
 class AssetManager {
@@ -15,12 +17,24 @@ public:
         bool hasTintColor;
     };
 
+    struct MeshInfo {
+    };
 public:
     AssetManager() = default;
 
     ~AssetManager() = default;
 
-    void registerMaterial(const std::string &uri, const Renderer::MaterialRef& ref, MaterialInfo materialInfo) {
+    // ------------------------------------ Meshes ---------------------------------------------------------------------
+    void registerMesh(const std::string &uri, const std::shared_ptr<Renderer::RenderMesh> &ref, MeshInfo meshInfo) {
+        meshes.emplace(uri, std::make_pair(ref, meshInfo));
+    }
+
+    [[nodiscard]] Renderer::RenderMesh &getMesh(const std::string &uri) const { return *meshes.at(uri).first; }
+
+    [[nodiscard]] MeshInfo getMeshInfo(const std::string &uri) const { return meshes.at(uri).second; }
+
+    // ------------------------------------ Materials ------------------------------------------------------------------
+    void registerMaterial(const std::string &uri, const Renderer::MaterialRef &ref, MaterialInfo materialInfo) {
         materials.emplace(uri, std::make_pair(ref, materialInfo));
     }
 
@@ -29,6 +43,7 @@ public:
     [[nodiscard]] MaterialInfo getMaterialInfo(const std::string &uri) const { return materials.at(uri).second; }
 
 private:
+    std::unordered_map<std::string, std::pair<std::shared_ptr<Renderer::RenderMesh>, MeshInfo>> meshes{};
     std::unordered_map<std::string, std::pair<Renderer::MaterialRef, MaterialInfo>> materials{};
 };
 
