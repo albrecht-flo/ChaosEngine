6389bcad2b69e1903f008009472e9c31a95c5a8a
Florian Albrecht <florian.t.albrecht@tum.de>
2022-03-26 12:49:16 +0100

    WIP: Added glyph information

--- Engine/src/core/uiSystem/FontManager.cpp
+++ Engine/src/core/uiSystem/FontManager.cpp
@@ -12,7 +12,24 @@
 
 using namespace ChaosEngine;
 
-std::shared_ptr<Font> FontManager::Create(const std::vector<FontManager::FontDefinition> &fontParts) {
+// For testing
+[[maybe_unused]] static void
+renderUVRect(const glm::vec2 &uvSize, const glm::vec2 &uvOffset, unsigned char *bitmap, uint32_t size) {
+    const glm::uvec2 topLeft = uvOffset * (float) size;
+    const glm::uvec2 extend = uvSize * (float) size;
+
+    uint32_t xOffset = topLeft.x;
+    uint32_t yOffset = topLeft.y * size;
+    for (int x = 0; x < extend.x; ++x) {
+        for (int y = 0; y < extend.y; ++y) {
+            bitmap[xOffset + x + yOffset + y * size] = (x < 1 || x >= extend.x - 1) ? 255 : bitmap[xOffset + x +
+                                                                                                   yOffset + y * size];
+        }
+    }
+}
+
+std::shared_ptr<Font>
+FontManager::Create(const std::string &name, const std::vector<FontManager::FontDefinition> &fontParts) {
     assert("List of font file MUST NOT be empty!" && !fontParts.empty());
 
     const std::string ttfFile = fontParts[0].ttfFile;
@@ -42,26 +59,27 @@ std::shared_ptr<Font> FontManager::Create(const std::vector<FontManager::FontDef
         throw std::runtime_error("ERROR::FREETYPE: Failed to set pixel size");
     }
 
+    std::map<wchar_t, Font::CharacterGlyph> charGlyphs;
     FT_UInt cIndex = 0;
     uint32_t i = 0;
     auto ftChar = FT_Get_First_Char(face, &cIndex);
-    const uint32_t yBaseOffset = padding * width;
+    const uint32_t yBaseOffset = padding;
     const uint32_t xBaseOffset = padding;
     while (ftChar != 0) {
         LOG_INFO("Found character {} in font", ftChar);
         if (FT_Load_Char(face, ftChar, FT_LOAD_RENDER)) {
             throw std::runtime_error("ERROR::FREETYPE: Failed to load glyph");
         }
+        const uint32_t yOffset = yBaseOffset + (i / mapSize) * glyphSize +
+                                 (i / mapSize) * padding;
+        const uint32_t xOffset = xBaseOffset + (i % mapSize) * glyphSize +
+                                 (i % mapSize) * padding;
         if (face->glyph->bitmap.buffer != nullptr) {
             LOG_INFO("Rendered character {} in font. {}x{}", ftChar, face->glyph->bitmap.width,
                      face->glyph->bitmap.rows);
-            const uint32_t yOffset = yBaseOffset + (i / mapSize) * (glyphSize * width) +
-                                     (i / mapSize) * padding * width;
-            const uint32_t xOffset = xBaseOffset + (i % mapSize) * glyphSize +
-                                     (i % mapSize) * padding;
-            const uint32_t baseOffset = yOffset + xOffset;
+            const uint32_t memoryOffset = yOffset * width + xOffset;
             for (uint32_t row = 0; row < face->glyph->bitmap.rows; ++row) {
-                const uint32_t offset = baseOffset + row * width;
+                const uint32_t offset = memoryOffset + row * width;
                 const uint32_t glyphBufferOffset = row * face->glyph->bitmap.width;
                 std::memcpy(mapBuffer.get() + offset, face->glyph->bitmap.buffer + glyphBufferOffset,
                             face->glyph->bitmap.width);
@@ -69,17 +87,24 @@ std::shared_ptr<Font> FontManager::Create(const std::vector<FontManager::FontDef
         } else {
             LOG_INFO("Skipping non renderable character {} in font", ftChar);
         }
+        charGlyphs.insert_or_assign(ftChar, Font::CharacterGlyph{
+                .uvSize = glm::vec2((float) face->glyph->bitmap.width / ((float) width),
+                                    (float) face->glyph->bitmap.rows / ((float) width)),
+                .uvOffset = glm::vec2((float) xOffset / ((float) width),
+                                      (float) yOffset / ((float) width)),
+                .advance = static_cast<uint32_t>(face->glyph->advance.x),
+        });
         // Next
         ftChar = FT_Get_Next_Char(face, ftChar, &cIndex);
         ++i;
     }
     // Append fallback Glyph
     {
-        const uint32_t yOffset = yBaseOffset / 2 + (i / mapSize) * (glyphSize * width) +
-                                 (i / mapSize) * padding * width;
+        const uint32_t yOffset = yBaseOffset / 2 + (i / mapSize) * glyphSize +
+                                 (i / mapSize) * padding;
         const uint32_t xOffset = xBaseOffset + (i % mapSize) * glyphSize +
                                  (i % mapSize) * padding;
-        const uint32_t baseOffset = yOffset + xOffset;
+        const uint32_t baseOffset = yOffset * width + xOffset;
         const auto localPadding = (uint32_t) std::ceil(glyphSize * 0.1);
         const auto borderWidth = (uint32_t) std::ceil(std::sqrt(glyphSize) / 2.0);
         for (uint32_t row = localPadding; row < glyphSize - localPadding; ++row) {
@@ -94,50 +119,32 @@ std::shared_ptr<Font> FontManager::Create(const std::vector<FontManager::FontDef
                 for (auto cell = localPadding; cell < glyphSize - localPadding; ++cell) {
                     const uint32_t offset = baseOffset + row * width + cell;
                     mapBuffer.get()[offset] = (cell < localPadding + borderWidth ||
-                                               cell > glyphSize - localPadding - borderWidth) ? 255 : 0;
+                                               cell >= glyphSize - localPadding - borderWidth) ? 255 : 0;
                 }
             }
         }
+        charGlyphs.insert_or_assign(0, Font::CharacterGlyph{
+                .uvSize = glm::vec2((float) (glyphSize - 2 * localPadding) / ((float) width),
+                                    (float) (glyphSize - 2 * localPadding) / ((float) width)),
+                .uvOffset = glm::vec2((float) (xOffset + localPadding) / ((float) width),
+                                      (float) (yOffset + localPadding) / ((float) width)),
+                .advance = glyphSize - 2 * localPadding,
+        });
     }
 
-    // TODO: Glyph boxes and test if they align
-    // TODO: Placeholder
     // TEST ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//    for (int i = 0; i < 48; ++i) {
-//        for (int j = 0; j < 48; ++j) {
-//            mapBuffer.get()[i*width + j] = 255;
-//        }
+//    for (int i = 0; i < width * width; ++i) {
+//        mapBuffer.get()[i] /= 2;
 //    }
+//    for (const auto &x: charGlyphs) {
+//        renderUVRect(x.second.uvSize, x.second.uvOffset, mapBuffer.get(), width);
+//    }
+//    stbi_write_png("out.png", width, width, 1, mapBuffer.get(), width);
     // TEST ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
 
-    // TEST
-    stbi_write_png("out.png", width, width, 1, mapBuffer.get(), width);
 
-    /*
-    // Load meta file
-    auto metaFile = AssetLoader::loadString(ttfFile + ".meta");
-    std::vector<int> characters;
-    std::istringstream iss(metaFile);
-    for (std::string line; std::getline(iss, line);) {
-        char *end = nullptr;
-        long car = std::strtol(line.c_str(), &end, 10);
-        if (end != line.c_str()) // Skip header line
-            characters.push_back(car);
-        if(car > 40)
-            break;
-    }
-    // Check if meta information contains errors / inconsistencies
-    LOG_INFO("Meta info contains {} glyphs", characters.size());
-    for (int c: characters) {
-        auto res = stbtt_FindGlyphIndex(&fontInfo, c);
-        if (res == 0) {
-            LOG_WARN("Could not find glyph from meta file charset {:x}", (uint32_t) c);
-        }
-    }
-     */
-
-    return nullptr;
+    return std::make_shared<Font>(name, style, glyphSize, std::move(charGlyphs), std::move(mapBuffer));
 }
--- Engine/src/core/uiSystem/FontManager.h
+++ Engine/src/core/uiSystem/FontManager.h
@@ -1,15 +1,38 @@
 #pragma once
 
+#include "glm/glm.hpp"
+
 #include <memory>
 #include <vector>
 #include <string>
+#include <map>
 
 namespace ChaosEngine {
 
-    enum class FontStyle {Regular, Italic, Bold};
+    enum class FontStyle {
+        Regular, Italic, Bold
+    };
 
     class Font {
+        friend class FontManager;
 
+    private:
+        struct CharacterGlyph {
+            glm::vec2 uvSize;
+            glm::vec2 uvOffset;
+            uint32_t advance;
+        };
+    public:
+        Font(const std::string &name, FontStyle style, uint32_t size, std::map<wchar_t, CharacterGlyph> &&glyphs,
+             std::unique_ptr<unsigned char[]> &&bitmap) :
+                name(name), style(style), size(size), glyphs(std::move(glyphs)), bitmap(std::move(bitmap)) {}
+
+    private:
+        const std::string &name;
+        FontStyle style;
+        uint32_t size;
+        std::map<wchar_t, CharacterGlyph> glyphs;
+        std::unique_ptr<unsigned char[]> bitmap;
     };
 
     class FontManager {
@@ -19,7 +42,8 @@ namespace ChaosEngine {
             FontStyle style;
         };
     public:
-        static std::shared_ptr<Font> Create(const std::vector<FontManager::FontDefinition>& fontParts);
+        static std::shared_ptr<Font>
+        Create(const std::string &name, const std::vector<FontManager::FontDefinition> &fontParts);
     };
 
 }
